1457528cd2d009fd0681d1b3b9979fd6c5de300c
Rafa <raf141.92@gmail.com>
2017-12-18 11:46:47 +0100

    Documentacion metodos

--- Hoja1_Estaticos_1/src/Metodos.java
+++ Hoja1_Estaticos_1/src/Metodos.java
@@ -5,8 +5,14 @@
  */
 
 public class Metodos {
+
 	private static final double PI = 3.141592;
 
+	/**
+	 * Método para calcular si un numero es primo o no
+	 * @param numero número ingresado por el usuario
+	 * @return true si es primo o false si no lo es
+	 */
 	public static boolean esPrimo(int numero) {
 		// Un numero menor o igual que 1, no es primo.
 		if (numero <= 1) {
@@ -28,6 +34,11 @@ public class Metodos {
 		}
 	}// esPrimo
 
+	/**
+	 * Método para calcular las cifras de un numero
+	 * @param numero número ingresado por el usuario
+	 * @return contador es el numero de cifras de @param
+	 */
 	public static int cuentaCifras(int numero) {
 		int contador = 0;
 		for (int i = numero; i > 0; i /= 10) {
@@ -37,6 +48,12 @@ public class Metodos {
 		return contador;
 	}// cuentaCifras
 
+
+	/**
+	 * Método para calcular los divisores de un numero
+	 * @param numero número ingresado por el usuario
+	 * @return cont es el numero de divisores de @param
+	 */
 	public static int cuentaDivisores(int numero) {
 		int cont = 0;
 		for (int divisor = (int) Math.sqrt(numero); divisor > 1; divisor--) {
@@ -48,6 +65,11 @@ public class Metodos {
 		return cont;
 	}// CuentaDivisores
 
+	/**
+	 * Método para calcular el factorial de un numero
+	 * @param numero número ingresado por el usuario
+	 * @return factorial es el factorial iterativo
+	 */
 	public static double factorial(int numero) {
 		double factorial = 1;
 		for (int i = 1; i <= numero; i++) {
@@ -56,21 +78,41 @@ public class Metodos {
 		return factorial;
 	}// Factorial iterativo
 
+
+	/**
+	 * Método para calcular el Maximo Comun Divisor
+	 * @param num1 primer número ingresado por el usuario
+	 * @param num2 segundo número ingresado por el usuario
+	 * @return MCD que es el Maximo Comun Divisor entre num1 y num2
+	 */
 	static int mcd(int numero1, int numero2) {
 		if (numero2 == 0)
 			return numero1;
 		else
 			return mcd(numero2, numero1 % numero2);
-	}// MCD recursivo
+	}// Maximo comun divisor recursivo
 
+
+	/**
+	 * Método para calcular el Mínimo Común Múltiplo
+	 * @param num1 primer número ingresado por el usuario
+	 * @param num2 segundo número ingresado por el usuario
+	 * @return mcm que es el Mínimo Común Múltiplo entre num1 y num2
+	 */
 	public static int mcm(int num1, int num2) {
 		int mcm = 0;
 		int a = Math.max(num1, num2);
 		int b = Math.min(num1, num2);
 		mcm = (a / mcd(a, b)) * b;
 		return mcm;
-	}// mcm recursivo
+	}// minimo comun multiplo recursivo
 
+	/**
+	 * Método para calcularlongitud, superficie o volumen de figuras
+	 * @param1 radio radio ingresado por el usuario
+	 * @param2 opcion 1,2,3 para el switch case
+	 * @return resultado longitud, superficie o volumen
+	 */
 	public static double radio(int radio, int opcion) {
 		double resultado;
 		resultado = 0;
@@ -88,12 +130,25 @@ public class Metodos {
 		return resultado;
 	}// Radio
 
+	/**
+	 * Método para calcular combinaciones de num1 sobre num2
+	 * @param1 num1 primer número ingresado por el usuario
+	 * @param2 num2 segundo número ingresado por el usuario
+	 * @return combinaciones es el numero de combinaciones de num1 sobre num2
+	 */
 	public static double combinaciones(int num1, int num2) {
 		double combinaciones = 0;
 		combinaciones = factorial(num1) / (factorial(num2) * factorial(num1 - num2));
 		return combinaciones;
 	}// combinaciones
 
+	/**
+	 * Método para calcular raices en una ecuacion 2G
+	 * @param1 num1 primer número ingresado por el usuario (a)
+	 * @param2 num2 segundo número ingresado por el usuario (b)
+	 * @param3 num3 segundo número ingresado por el usuario (c)
+	 * @return raiz2 de la ecuacion 2G
+	 */
 	public static double Ecuacion2G(int a, int b, int c) {
 		double raiz1, raiz2;
 		raiz1 = (-b + Math.sqrt((Math.pow(b, 2)) - (4 * a * c))) / (2 * a);
