7a9693c4c3d72ffbc952225ae2d28f5f8fe9d436
Rafa <raf141.92@gmail.com>
2018-01-10 08:21:13 +0100

    Hora segundos

--- Hoja1_Ejercicio2-Reloj/src/Hora.java
+++ Hoja1_Ejercicio2-Reloj/src/Hora.java
@@ -69,6 +69,7 @@ public class Hora {
 			minutos = minutos % 60;
 		}
 		System.out.printf("%02d:%02d:%02d", horas % 24, minutos, segundos);
+		System.out.println("\nLa suma de la hora en segundos es: " +(horas*3600+minutos*60+segundos));
 		return hora;
 	}
 
