7f464ff277c1af155d020f11191c75fac8a303f2
Rafa <raf141.92@gmail.com>
2018-01-25 23:03:31 +0100

    Metodos clase paises

--- HojaRepaso_Paises/src/ListaDePaises.java
+++ HojaRepaso_Paises/src/ListaDePaises.java
@@ -15,28 +15,49 @@ public class ListaDePaises {
 		paises = new String[numeroPaises];
 	}
 
-
 	public String[] getPaises() {
 		return paises;
 	}
-
 
 	public void setPaises(String[] paises) {
 		this.paises = paises;
 	}
 
-
 	public static int getContadorPaises() {
 		return contadorPaises;
 	}
 
-	public static boolean anadir(String nombre) {
-		// ara añadir a la lista un nombre. Devuelve true si lo ha podido
+	public  boolean anadir(String nombre) {
+		int contador=0;
+		boolean anadido=false;
+		// para añadir a la lista un nombre. Devuelve true si lo ha podido
 		// añadir y false en caso contrario.
-		return;
+		if (!existeNombre(nombre)) {
+			anadido = true;
+			paises[contador]=nombre;
+		}
+		contador++;
+		return anadido;
+	}
+	@Override
+	public String toString() {
+		return "ListaDePaises [paises=" + Arrays.toString(paises) + "]";
 	}
 
-	public static boolean borrar(String nombre) {
+	public  boolean existeNombre(String nombre) {
+		// para añadir a la lista un nombre. Devuelve true si lo ha podido
+		// añadir y false en caso contrario.
+		boolean existencia=false;
+		for (int i = 0; i < paises.length; i++) {
+			//Si uno coincide devuelve true
+			if (paises[i]!=null && paises[i].equals(nombre)) {
+				existencia=true;
+			}
+		}
+
+		return existencia;
+	}
+	public boolean borrar(String nombre) {
 		// Devuelve true si ha podido borrar el nombre que recibe en el
 		// parámetro y false en caso contrario. Al borrar un nombre hay que
 		// desplazar un lugar a la izquierda
@@ -45,29 +66,51 @@ public class ListaDePaises {
 		return;
 	}
 
-	public static void vaciarLista() {
-		// Borra todos los nombres de la lista.
+	public void vaciarLista() {
+		for (int i = 0; i < paises.length; i++) {
+			paises[i] = null;
+		}
+		paises[numeroPaises] = paises[0];
 	}
 
-	public static String anadir(int posicion) {
-		//Devuelve el nombre que se encuentra en la posición pasada como
-		//parámetro (la primera es la posición 0). Devuelve null si no es una posición válida.
-		return;
+	public String mostrar(int posicion) {
+		// Devuelve el nombre que se encuentra en la posición pasada como
+		// parámetro (la primera es la posición 0). Devuelve null si no es una
+		// posición válida.
+		for (int i = 0; i < paises.length; i++) {
+			if (paises[posicion] != null) {
+				posicion = i;
+			} else {
+				return "null";
+			}
+		}
+		return paises[posicion];
+	}// mostrar
+
+
+	public Integer numElementos() {
+		// Devuelve el número de elementos que se almacenan en el vector
+		Integer contador = 0;
+		for (int i = 0; i < paises.length; i++) {
+			if (paises[i] != null) {
+				contador++;
+			}
+		}
+		return contador;
 	}
 
-	public static Integer numElementos() {
-		//Devuelve el número de elementos que se almacenan en el vector.
-		return;
+	public Integer maxElementos() {
+		return numeroPaises;
 	}
 
-	public static Integer maxElementos() {
-		//Devuelve el número máximo de elementos que podemos guardar en el vector.
-		return;
+	public boolean estaLlena() {
+		boolean listaLLena = false;
+		for (int i = 0; i < paises.length; i++) {
+			//Si el ultimo elemento no es null está vacia
+			if (paises[i] != null) {
+				listaLLena = true;
+			}
+		}
+		return listaLLena;
 	}
-
-	public static boolean estaLlena() {
-		//Si ya no se puede guardar ningún nombre más devuelve true, en caso
-		//contrario devuelve false
-		return;
-	}
-}
+}//Class
