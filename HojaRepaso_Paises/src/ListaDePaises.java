34cc9cbbdc58582dfe2bd64ff1a44ae45b0c1064
Rafa <raf141.92@gmail.com>
2018-01-25 13:14:58 +0100

    paises pl

--- HojaRepaso_Paises/src/ListaDePaises.java
+++ HojaRepaso_Paises/src/ListaDePaises.java
@@ -7,58 +7,27 @@ public class ListaDePaises {
 	 * vector. Los nombres se almacenarán en elementos consecutivos del vector.
 	 */
 	private Integer numeroPaises;
-	private String nombrePais;
+	private String[] nombrePais;
 	public static int contadorPaises = 0;
-
 	/**
 	 * @param numeroPaises
+	 * @param nombrePais
 	 */
-	public ListaDePaises (Integer numeroPaises) {
+	public ListaDePaises(Integer numeroPaises) {
 		this.numeroPaises = numeroPaises;
-		this.nombrePais = 
-		contadorPaises++;
+		this.nombrePais = nombrePais;
 	}
+	public String[] getNombrePais() {
+		return nombrePais;
+	}
+	public void setNombrePais(String[] nombrePais) {
+		this.nombrePais = nombrePais;
+	}
+	public static int getContadorPaises() {
+		return contadorPaises;
+	}
+
 	
-	public Boolean anadir(String nombre) {
-		// Para añadir a la lista un nombre. Devuelve true si lo ha podido
-		// añadir y false en caso contrario.
-		for (int i = 0; i < numeroPaises; i++) {
-			if (ListaDePaises[i].equals(nombre)) {
-				
-			}
-		}
-	}
 
-	public Boolean borrar(String nombre) {
-		// Devuelve true si ha podido borrar el nombre que recibe en el
-		// parámetro y false en caso contrario. Al borrar un nombre hay que
-		// desplazar un lugar a la izquierda todos los elementos que quedan a la
-		// derecha del nombre que se ha borrado.
-
-	}
-
-	public void vaciarLista() {
-		// Borra todos los nombres de la lista.
-	}
-
-	public String mostrar(int posicion) {
-		// Devuelve el nombre que se encuentra en la posición pasada como
-		// parámetro (la primera es la posición 0). Devuelve null si no es una
-		// posición válida.
-	}
-
-	public Integer numElementos() {
-		// Devuelve el número de elementos que se almacenan en el vector.
-	}
-
-	public Integer maxElementos() {
-		// Devuelve el número máximo de elementos que podemos guardar en el
-		// vector.
-	}
-
-	public Boolean estaLlena() {
-		// Si ya no se puede guardar ningún nombre más devuelve true, en caso
-		// contrario devuelve false.
-
-	}
+	
 }
