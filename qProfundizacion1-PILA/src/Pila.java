39eaa04a1a0f96cf07478ce3a573c86ba028b89f
Rafa <raf141.92@gmail.com>
2018-02-15 00:22:07 +0100

    Pila to-do v3

--- qProfundizacion1-PILA/src/Pila.java
+++ qProfundizacion1-PILA/src/Pila.java
@@ -1,42 +1,57 @@
 
 public class Pila {
 
-	private Integer[] monton;
-	private Integer numeroDeCosas;
-	private static int contaCosas = 0;
+	private int[] datos;
+	private static int contadorDatos = 0;
 
-	public Pila(Integer numeroDeCosas) {
-		this.numeroDeCosas = numeroDeCosas;
-		monton = new Integer[numeroDeCosas];
+	public Pila() {
+		datos = new int[500];
 	}
 
+	/**
+	 * añadir un elemento a la PILA (encima, lógicamente) (ADD)
+	 * 
+	 * @param valor
+	 */
 	public void apilar(int valor) {
 
 	}
 
+	/**
+	 * quitar un elemento de la PILA mostrándolo en pantalla (POP)
+	 * 
+	 * @param valor
+	 */
 	public void desaapilar(int valor) {
 
 	}
 
-	public boolean estaLlena(){
-		if (contaCosas == monton.length){
+	/**
+	 * Comprobar si esta llena
+	 * @return
+	 */
+	public boolean estaLlena() {
+		if (contadorDatos == datos.length) {
 			return true;
-		} else{
+		} else {
 			return false;
 		}
 	}
 
-	public void copy(Pila copia) {
-
+	/**
+	 * Copiar una pila en otra
+	 * 
+	 * @param copia
+	 */
+	public Pila copy(Pila unaPila) {
+		return unaPila;
 	}
 
 	/*
-	 * quitar un elemento de la PILA mostrándolo en pantalla (POP). añadir un
-	 * elemento a la PILA (encima, lógicamente) (ADD) . Copiar una pila en otra
-	 * (COPY). Invertir una PILA (INV), es decir, el elemento cima pasa a ser el
-	 * último y así sucesivamente hasta que el último esté el primero, es decir,
-	 * en la cima. La pila inicial desaparece y hay que crear un nuevo objeto
-	 * pila. Borrar toda la PILA. Método que nos indica con un boolean si la
-	 * PILA esta vacía o no
+	 * Invertir una PILA (INV), es decir, el elemento cima pasa a ser el último
+	 * y así sucesivamente hasta que el último esté el primero, es decir, en la
+	 * cima. La pila inicial desaparece y hay que crear un nuevo objeto pila.
+	 * Borrar toda la PILA. Método que nos indica con un boolean si la PILA esta
+	 * vacía o no
 	 */
 }
