4ea81bf81c3cc209484a6fc5b591e826fc6cf6bf
Rafa <raf141.92@gmail.com>
2018-02-15 07:40:12 +0100

    Pila+

---
+++ qProfundizacion1-PILA/src/GestionPila.java
@@ -0,0 +1,32 @@
+
+public class GestionPila {
+
+	public static void main(String[] args) {
+		int opcion;
+		opcion = Teclado.pedirEntero("Menu");
+		while (opcion != 0) {
+			switch (opcion) {
+			case 1:
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+
+			default:
+				break;
+			}// switch
+			opcion = Teclado.pedirEntero("Menu");
+		} // while
+	}// main
+
+}// Class
--- qProfundizacion1-PILA/src/Pila.java
+++ qProfundizacion1-PILA/src/Pila.java
@@ -1,52 +1,69 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class Pila {
 
 	private int[] datos;
-	private static int contadorDatos = 0;
+	private static int cima = 0;
 
 	public Pila() {
 		datos = new int[500];
+		cima = 0;
 	}
 
-	/**
-	 * añadir un elemento a la PILA (encima, lógicamente) (ADD)
-	 * 
-	 * @param valor
-	 */
-	public void apilar(int valor) {
-
+	public int apilar() {
+		Random rand = new Random();
+		datos[cima] = rand.nextInt(9 - 1 + 1) + 1;
+		cima++;
+		return cima;
 	}
 
-	/**
-	 * quitar un elemento de la PILA mostrándolo en pantalla (POP)
-	 * 
-	 * @param valor
-	 */
-	public void desaapilar(int valor) {
-
+	public int desApilar() {
+		datos[cima] = 0;
+		cima--;
+		return cima;
 	}
 
-	/**
-	 * Comprobar si esta llena
-	 * @return
-	 */
 	public boolean estaLlena() {
-		if (contadorDatos == datos.length) {
+		if (cima == datos.length) {
 			return true;
 		} else {
 			return false;
 		}
-	}
+	} // estaLlena
 
-	/**
-	 * Copiar una pila en otra
-	 * 
-	 * @param copia
-	 */
+	public boolean estaVacia() {
+		if (cima == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}// estaVacia
+
 	public Pila copy(Pila unaPila) {
+		int [] unosDatos;
+		Arrays.copyOf(datos, 500);
 		return unaPila;
 	}
 
+	public static int[] invArray(int[] array) {
+		int aux;
+		for (int i = 0; i < array.length / 2; i++) {
+			aux = array[i];
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = aux;
+		}
+		return array;
+	}
+
+	public String borrar(Pila unaPila) {
+		if (unaPila.estaVacia()) {
+			datos[500] = datos[0];
+			return "Pila borrada";
+		} else {
+			return "Pila no borrada";
+		}
+	}
 	/*
 	 * Invertir una PILA (INV), es decir, el elemento cima pasa a ser el último
 	 * y así sucesivamente hasta que el último esté el primero, es decir, en la
