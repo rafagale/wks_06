4850b1e7ae5156cb385ec4d03a1d44f0c1f77cad
Rafa <raf141.92@gmail.com>
2018-02-24 13:25:33 +0100

    interface

--- PH-Ejercicio8-Herencia/src/Persona.java
+++ PH-Ejercicio8-Herencia/src/Persona.java
@@ -1,14 +1,12 @@
 
 public interface Persona {
 
-	//Es static y final aunque no se ponga
-	public String nombreEmpresa="Endesa";
+	// Es static y final aunque no se ponga
+	public String nombreEmpresa = "Endesa";
 
-	//Es abstract aunque no lo ponga
+	// Es abstract aunque no lo ponga
 	public void pedirTodosDatos();
 
 	public void visualizarTodosDatos();
-	
-	
 
 }
